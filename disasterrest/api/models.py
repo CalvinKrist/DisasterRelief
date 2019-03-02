9efdb51cb98021bcfabb7cb95ed32f8cc59d0577
CalvinKrist <calvin.krist@yahoo.com>
2019-03-02 11:06:54 -0500

    model definitions completed

--- disasterrest/api/models.py
+++ disasterrest/api/models.py
@@ -5,8 +5,11 @@ class GeoLocation(models.Model):
     longitude = models.FloatField()
 
 class MediaAlert(models.Model):
-    source_type   = models.CharField(max_length=255)
-    url           = models.URLField()
-    date_created  = models.DateTimeField(blank=True)
-    location_name = models.CharField(max_length=255, blank=True)
-    geo_location  = GeoLocation()
+    source_type       = models.CharField(max_length=255)
+    url               = models.URLField()
+    date_created      = models.DateTimeField(blank=True)
+    location_name     = models.CharField(max_length=255, blank=True)
+    geo_location      = models.OneToOneField(GeoLocation, on_delete=models.SET_NULL, blank=True,
+                                             related_name='geo_location')
+    disaster_type     = models.CharField() # cannot be blank: label 'unknown' if not known
+    disaster_severity = models.CharField() # low, medium, high -- we ignore 'none' as best we can
