78a7325135a23c94116e1c6c970c435ee56c1128
CalvinKrist <calvin.krist@yahoo.com>
2019-03-03 09:11:25 -0500

    suff

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-import api.models.MediaAlert as MediaAlert
+from . models import MediaAlert
 
 @admin.register(MediaAlert)
 class MediaAmin(admin.ModelAdmin):
